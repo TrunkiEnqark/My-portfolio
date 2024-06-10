200ade769cbd5b87244c57bd6d4844f48c04d6a8
Trunki Enqark <trunki.enqark@gmail.com>
2024-06-10 12:02:02 +0700

    update

--- script.js
+++ script.js
@@ -2,37 +2,49 @@ let lastScrollTop = 0;
 let navbar = document.querySelector('header');
 let navLinks = document.querySelectorAll('header nav a');
 let sections = document.querySelectorAll('section');
-let navigator = document.querySelector('navbar');
+// let menu = document.querySelector('navbar');
 
-window.addEventListener("scroll", () => {
-    var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navbar.classList.add('hidden');
-    } else {
-        navbar.classList.remove('hidden');
-    }
-    lastScrollTop = scrollTop;
-})
+// window.addEventListener("scroll", () => {
+//     var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop) {
+//         navbar.classList.add('hidden');
+//     } else {
+//         navbar.classList.remove('hidden');
+//     }
+//     lastScrollTop = scrollTop;
+// })
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
+        let scrolledY = window.scrollY;
         let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                if (links) {
-                    links.classList.remove('active');
-                }
-                let el = document.querySelector('header nav a [href*=' + id + ']');
-                el.classList.add('active');
-            })
+        let height = sec.offsetHeight;
+        let scrollTop = sec.offsetTop;
+        console.log(scrolledY + ' ' + scrollTop + ' ' + height + ' ' + id)
+        if (scrollTop <= scrolledY && scrolledY < scrollTop + height) {
+            removeActiveClasses();
+            activeClassById(id);
         }
-    });
+    })
 }
 
-navigator.onclick = () => {
-    navigator.classList.toggle('active');
+navLinks.forEach(links => {
+    links.addEventListener('click', () => {
+        removeActiveClasses();
+        links.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    navLinks.forEach(links => {
+        links.classList.remove('active')
+    })
+}
+
+function activeClassById(id) {
+    navLinks.forEach(links => {
+        if (links.href.includes(id)) {
+            links.classList.toggle('active');
+        }
+    })
 }
